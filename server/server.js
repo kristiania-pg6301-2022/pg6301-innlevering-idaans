b62b538cba678a6ef377aeff3f0cbb03d4c76b2c
Jannicke <jannicke.hamarsnes@gmail.com>
2022-02-28 12:41:54 +0100

    setup prettier and husky

--- server/server.js
+++ server/server.js
@@ -3,15 +3,15 @@ import express from "express";
 const app = express();
 
 app.get("login", (req, res, next) => {
-    res.json({username: "Noen andre person"})
+  res.json({ username: "Noen andre person" });
 });
 
 app.post("login", (req, res, next) => {
-    res.sendStatus(401)
+  res.sendStatus(401);
 });
 
 app.use(express.static("../client/dist"));
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Started on http://localhost:${server.address().port}`);
+  console.log(`Started on http://localhost:${server.address().port}`);
 });
