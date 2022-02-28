552bed4b332f4c56624318d86e5da2ec924a87c7
idaans <idaa579@gmail.com>
2022-02-28 12:25:15 +0100

    launch

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ import express from "express";
 const app = express();
 
 app.get("login", (req, res, next) => {
+    res.json({username: "Noen andre person"})
+});
+
+app.post("login", (req, res, next) => {
     res.sendStatus(401)
-})
+});
+
+app.use(express.static("../client/dist"));
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Started on http://localhost:${server.address().port}`);
