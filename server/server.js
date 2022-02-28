fd5fd837b6da83e38f68369273fd708d7aa609d0
Jannicke <jannicke.hamarsnes@gmail.com>
2022-02-28 11:12:38 +0100

    Initial project

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("login", (req, res, next) => {
+    res.sendStatus(401)
+})
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Started on http://localhost:${server.address().port}`);
+});
