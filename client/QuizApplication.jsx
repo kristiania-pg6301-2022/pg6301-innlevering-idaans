bb0dfbcf73264210efc7ad80a10346d8884d2715
jannickeham <jannicke.hamarsnes@gmail.com>
2022-03-02 13:26:48 +0100

    Setup react-router application and added NewQuiz and FrontPage with routing

--- client/QuizApplication.jsx
+++ client/QuizApplication.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function handleNavigation(e) {
+  let navigate = useNavigate();
+  let path = "newquiz";
+  navigate(path);
+}
 
 export function ShowQuiz() {
   return (
     <>
-      <h1>Quiz</h1>
-      <button>Take quiz</button>
+      <h1>Welcome to the quiz page</h1>
+      <Link to="/newquiz">
+        <button>Take new quiz</button>
+      </Link>
     </>
   );
 }
--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ShowQuiz } from "./QuizApplication";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<ShowQuiz />, document.getElementById("app"));
+function Frontpage() {
+  return (
+    <>
+      <ShowQuiz />
+    </>
+  );
+}
+
+function NewQuiz() {
+  return (
+    <>
+      <h1>This is new quiz page</h1>
+      <h3>Heres question</h3>
+
+      <button>Generate new random question</button>
+    </>
+  );
+}
+
+export function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Frontpage />} />
+        <Route path="/newquiz" element={<NewQuiz />} />
+        <Route path="/*" element={<h1>Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
